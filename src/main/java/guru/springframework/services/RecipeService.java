e03f57a3afd9b2701f18795f113d5291ffbe258c
John Thompson <john@springframework.guru>
2017-06-21 20:15:13 -0400

    adding command objects, type conveters, tests, and service method
    # Conflicts:
    #	src/main/java/guru/springframework/services/RecipeService.java
    #	src/main/java/guru/springframework/services/RecipeServiceImpl.java
    #	src/test/java/guru/springframework/services/RecipeServiceImplTest.java

--- src/main/java/guru/springframework/services/RecipeService.java
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -1,38 +1,18 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.model.Recipe;
-import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
 @Service
-public class RecipeService  {
+public interface RecipeService  {
 
-    private RecipeRepository recipeRepository;
 
-    public RecipeService(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
-    }
-
-    public Set<Recipe> getRecipes() {
-        log.debug("In the recipe service...");
-
-        Set<Recipe> recipeSet = new HashSet<>();
-        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
-        return recipeSet;
-    }
-
-    public Recipe getRecipeById(Long id) {
-        Optional<Recipe> recipe = recipeRepository.findById(id);
-
-        if (recipe.isPresent())
-            return recipe.get();
-
-        return null;
-    }
+    Set<Recipe> getRecipes();
+    Recipe findById(Long l);
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
