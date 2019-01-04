e0081803df065d2a4fc0ba52928031157b142eaf
DADDC003 <christopher.daddario@espn.com>
2019-01-04 11:00:11 -0500

    added jpa query methods to repositories and correct indexcontroller package name

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,15 +1,36 @@
-package com.dadarioc.spring.recipe.controllers;
+package guru.springframework.controllers;
 
-import org.springframework.stereotype.Component;
-import org.springframework.ui.Model;
+import guru.springframework.model.Category;
+import guru.springframework.model.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component
+import java.util.Optional;
+
+/**
+ * Controller class for main index landing page
+ */
+@Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index"})
-    public String getIndexPage(Model model) {
-        model.addAttribute("index");
+    public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Italian");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure ID is: " + unitOfMeasureOptional.get().getId());
 
         return "index";
     }
